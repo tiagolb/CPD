6b625b3372a355e479ab8c062d9dda428a92d797
Tiago Luis de Oliveira Brito <ist172647@linbase.rnl.tecnico.ulisboa.pt>
2014-10-28 18:03:39 +0000

    [RFT] Changed the names of the files to be compliant with the project statement

---
+++ MPI/lcs-mpi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/* DEBUG */
+
+//#define DEBUG
+#define DEBUG_TIME
+
+/* RETURN CONSTANTS */
+
+#define SUCCESS 0
+
+
+/* MAIN */
+
+int main(int argc, char* argv[]) {
+
+	std::cout << "Hello World!" << std::endl;
+	return SUCCESS;
+}
